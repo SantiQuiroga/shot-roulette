b4f508e50e031e3a37a524fe372190c1e003d4c4
SantiQuiroga <quiroga.default.santiago@gmail.com>
2024-05-20 22:44:34 -0400

    improve

--- Roulette.py
+++ Roulette.py
@@ -4,7 +4,6 @@ import time
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 
-# Run with: python Roulette.py
 def show_message():
     while True:
         time.sleep(random.randint(0, 180))
@@ -13,8 +12,8 @@ def show_message():
 def ask_question():
     while True:
         time.sleep(random.randint(0, 180))
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 100)
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 10)
         operation = random.choice(['+', '-', '*'])
         if operation == '+':
             correct_answer = num1 + num2
@@ -25,10 +24,19 @@ def ask_question():
 
         user_answer = simpledialog.askinteger("Shot?", f"{num1} {operation} {num2}")
         if user_answer != correct_answer:
-            messagebox.showinfo("Wrong Bitch", "Shot.")
+            messagebox.showinfo("Wrong Bitch", "Drink a Shot.")
 
 threading.Thread(target=show_message, daemon=True).start()
 threading.Thread(target=ask_question, daemon=True).start()
 
 while True:
     time.sleep(1)
+
+# To Run: 
+# ./run.sh
+#  
+# To Stop:
+# pgrep -f Roulette.py
+#
+# Copy the ID it gives you and then:
+# kill -9 <ID>
